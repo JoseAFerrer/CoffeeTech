7252c74e229c56dbf74c6b2eb9579678937333e6
JoseAFerrer <seriamente.fs@gmail.com>
2026-01-14 00:03:36 +0100

    Implement use case 5

--- ListsJoyAndPain/Program.cs
+++ ListsJoyAndPain/Program.cs
@@ -9,3 +9,4 @@ UseCases.UC1_WhoHasMoreBooks();
 UseCases.UC2_WhoHasMoreCompleteSagasOutOfTheCommonOnes();
 UseCases.UC3_WhoHasMoreBooksInCommonSagas();
 UseCases.UC4_ExchangeCompleteCollections();
+UseCases.UC5_WhoHasMoreOfDifferentAuthors();
--- ListsJoyAndPain/Use cases/UseCase05.cs
+++ ListsJoyAndPain/Use cases/UseCase05.cs
@@ -9,5 +9,28 @@ public partial class UseCases
     {
         Quick.InsertUseCaseTitle(5);
         var (bookshopA, bookshopB) = BookshopService.GetBookshops();
-    }
+        
+        var authorIds = bookshopA
+            .Sagas.SelectMany(x => x.AuthorIds)
+            .Concat(bookshopB.Sagas.SelectMany(x => x.AuthorIds))
+            .Distinct()
+            .ToArray();
+
+        var dictOfAuthors = new Dictionary<string, (int a, int b)>();
+        foreach (var authorId in authorIds)
+        {
+            var countA = bookshopA.Sagas
+                .Where(x => x.AuthorIds.Contains(authorId))
+                .Sum(x => x.Size);
+            var countB = bookshopB.Sagas
+                .Where(x => x.AuthorIds.Contains(authorId))
+                .Sum(x => x.Size);
+            dictOfAuthors.Add(authorId, (countA, countB));
+        }
+        
+        var authorsWhereAHasMoreBooks = dictOfAuthors.Count(pair => pair.Value.a > pair.Value.b);
+        var authorsWhereBHasMoreBooks = dictOfAuthors.Count(pair => pair.Value.a < pair.Value.b);
+        Console.WriteLine($"Bookshop A has more books for {authorsWhereAHasMoreBooks} common authors");
+        Console.WriteLine($"Bookshop B has more books for {authorsWhereBHasMoreBooks} common authors");
+   }
 }
