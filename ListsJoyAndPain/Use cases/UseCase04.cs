5f30692228b5f86950b14c317b30940fb33b1495
JoseAFerrer <seriamente.fs@gmail.com>
2026-01-13 23:43:16 +0100

    Cleanup use case 4

--- ListsJoyAndPain/Use cases/UseCase04.cs
+++ ListsJoyAndPain/Use cases/UseCase04.cs
@@ -1,4 +1,5 @@
 using ListsJoyAndPain.Bookshop;
+using ListsJoyAndPain.Models;
 using ListsJoyAndPain.Utilities;
 
 namespace ListsJoyAndPain.Use_cases;
@@ -13,8 +14,28 @@ public partial class UseCases
         var completeSagasInA = bookshopA.Sagas.Where(x => x.IsComplete).ToArray();
         var completeSagasInB = bookshopB.Sagas.Where(x => x.IsComplete).ToArray();
         
+        var (completeInANotCompleteInB,
+            completeInBNotCompleteInA,
+            incompleteToRemoveFromBAndAddToA,
+            incompleteToRemoveFromAAndAddToB) = BuildSetsToWork(completeSagasInA, completeSagasInB, bookshopB, bookshopA);
+
+        UpdateBookshops(bookshopA, completeInANotCompleteInB, incompleteToRemoveFromAAndAddToB, completeInBNotCompleteInA, incompleteToRemoveFromBAndAddToA, bookshopB);
+
+        BookshopService.SaveBookshops(bookshopA, bookshopB);
+
+        Console.WriteLine("Calling use case 2 to see if numbers are reversed:");
+        UC2_WhoHasMoreCompleteSagasOutOfTheCommonOnes();
+    }
+
+    private static (Saga[] completeInANotCompleteInB,
+        Saga[] completeInBNotCompleteInA,
+        Saga[] incompleteToRemoveFromBAndAddToA,
+        Saga[] incompleteToRemoveFromAAndAddToB) BuildSetsToWork(Saga[] completeSagasInA,
+            Saga[] completeSagasInB, Models.Bookshop bookshopB, Models.Bookshop bookshopA)
+    {
         // Note: this does not work since they are complex objects & we are not using a custom EqualityComparer
-        // var test = completeSagasInB.Except(completeSagasInA).ToArray();
+        // var completeInBNotCompleteInA = completeSagasInB.Except(completeSagasInA).ToArray();
+        
         
         var completeInANotCompleteInB = completeSagasInA
             .ExceptBy(completeSagasInB.Select(s => s.Id), s => s.Id).ToArray();
@@ -24,11 +45,19 @@ public partial class UseCases
         var incompleteToRemoveFromBAndAddToA = bookshopB.Sagas
             .Where(x => completeInANotCompleteInB.Select(y => y.Id)
                 .Contains(x.Id)).ToArray();
-
         var incompleteToRemoveFromAAndAddToB = bookshopA.Sagas
             .Where(x => completeInBNotCompleteInA.Select(y => y.Id)
                 .Contains(x.Id)).ToArray();
-        
+        return (completeInANotCompleteInB, completeInBNotCompleteInA, incompleteToRemoveFromBAndAddToA, incompleteToRemoveFromAAndAddToB);
+    }
+
+    private static void UpdateBookshops(Models.Bookshop bookshopA,
+        Saga[] completeInANotCompleteInB,
+        Saga[] incompleteToRemoveFromAAndAddToB,
+        Saga[] completeInBNotCompleteInA,
+        Saga[] incompleteToRemoveFromBAndAddToA,
+        Models.Bookshop bookshopB)
+    {
         var updatedBooksInA = bookshopA.Sagas
             .Except(completeInANotCompleteInB)
             .Except(incompleteToRemoveFromAAndAddToB)
@@ -44,10 +73,5 @@ public partial class UseCases
             .Concat(completeInANotCompleteInB)
             .Concat(incompleteToRemoveFromAAndAddToB).ToList();
         bookshopB.Sagas = updatedBooksInB;
-        
-        BookshopService.SaveBookshops(bookshopA, bookshopB);
-
-        Console.WriteLine("Calling use case 2 to see if numbers are reversed:");
-        UC2_WhoHasMoreCompleteSagasOutOfTheCommonOnes();
     }
 }
